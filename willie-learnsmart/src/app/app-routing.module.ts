f20bf55d051486989feea5c1415c85b1e54ea61a
Olli T <92941044+OliverTheil@users.noreply.github.com>
2022-03-20 18:57:31 +0100

    001 - First questions and design

--- willie-learnsmart/src/app/app-routing.module.ts
+++ willie-learnsmart/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { QuestionsComponent } from './questions/questions.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'questions', component: QuestionsComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
