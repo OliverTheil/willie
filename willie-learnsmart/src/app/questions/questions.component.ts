cf462181bd2a5bb9c963341b0e9ca68ae9abde54
Olli T <92941044+OliverTheil@users.noreply.github.com>
2022-03-24 18:01:45 +0100

    005

--- willie-learnsmart/src/app/questions/questions.component.ts
+++ willie-learnsmart/src/app/questions/questions.component.ts
@@ -3,13 +3,41 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-questions',
   templateUrl: './questions.component.html',
-  styleUrls: ['./questions.component.scss']
+  styleUrls: ['./questions.component.scss'],
 })
 export class QuestionsComponent implements OnInit {
+  userName: string = '';
+  userAge: any = 0;
+  userTopic: string = '';
+  userGoal: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  createPlanBtn() {}
+
+  addName(userName: string) {
+    if (userName) {
+      this.userName = userName;
+    }
   }
 
+  addAge(userAge: string) {
+    if (userAge) {
+      this.userAge = userAge;
+    }
+  }
+
+  addTopic(userTopic: string) {
+    if (userTopic) {
+      this.userTopic = userTopic;
+    }
+  }
+
+  addGoal(userGoal: string) {
+    if (userGoal) {
+      this.userGoal = userGoal;
+    }
+  }
 }
