ffdc6cefe3a1ef8a6328fb3e5d3c2cafb2dfb510
Olli T <92941044+OliverTheil@users.noreply.github.com>
2022-03-29 12:03:58 +0200

    015 - more calculations etc

--- willie-learnsmart/src/app/models/answers.class.ts
+++ willie-learnsmart/src/app/models/answers.class.ts
@@ -1,20 +1,20 @@
 export class Answers {
   public userName: any = '';
-  public userAge: number = 0;
+  public userAge: number = 6;
   public userTopic: string = '';
   public userGoal: string = '';
 
   public workPerWeek: number = 0;
-  public lifePerDay: number = 2;
+  public lifePerDay: number = 120;
   public level: number = 1;
 
   public learningSlow: boolean = true;
   public learningEnduranceGood: boolean = false;
   public learningLove: boolean = false;
 
-  public minutesPerDay: number = 0;
+  public minutesPerDay: number = 30;
   public moreAtWeekends: boolean = false;
-  public maxHoursPerWeek: number = 2;
+  public maxMinutesPerWeek: number = 120;
 
   public notPossibleDays: string[] = [];
   public goodDays: string[] = [];
--- willie-learnsmart/src/app/plan/plan.component.ts
+++ willie-learnsmart/src/app/plan/plan.component.ts
@@ -13,12 +13,23 @@ export class PlanComponent implements OnInit {
   plan: Plan;
 
   maxDays: number = 7;
-  hoursPerWeek: number = 168;
-  hoursEssential: number = 56;
+  minutesPerWeek: number = 10080;
+  minutesEssential: number = 3360;
+  userAge: number = 0;
 
-  maxUserHours: number = 0;
+  /**
+   * * Standardfactor = 1. Stands for the learning ability. Smallest = 0.5, Highest 1.5.
+   */
+  userLearningAbility = 1;
+  levelFactor = 1;
+  ageFactor = 1;
+  feelingSpeed = 1;
+  feelingEndurance = 1;
+  feelingLove = 1;
+
+  maxUserMinutes: number = 0;
   maxUserDays: number = 7;
-  maxUserHoursPerDay: number = 0;
+  maxUserMinutesPerDay: number = 0;
 
   constructor() {}
 
@@ -31,48 +42,152 @@ export class PlanComponent implements OnInit {
   }
 
   calculationPlan() {
-    this.calculateHours();
-    this.calculateHoursPerDay();
+    this.calculateMinutes();
+    this.calculateMinutesPerDay();
     this.checkNotPossibleDays();
+    this.setUserAbility();
   }
 
-  calculateHours() {
-    let hoursMinusLife =
-      this.hoursPerWeek - this.answers.lifePerDay * 7 - this.hoursEssential;
-    let hoursMinusWork = hoursMinusLife - this.answers.workPerWeek - 5;
-    if (hoursMinusWork >= this.answers.maxHoursPerWeek) {
-      this.maxUserHours = this.answers.maxHoursPerWeek;
-    } else if (
-      hoursMinusWork < this.answers.maxHoursPerWeek &&
-      hoursMinusWork > 2
-    ) {
-      this.maxUserHours = hoursMinusWork;
-    } else if (this.maxUserHours <= 2) {
-      this.maxUserHours = 2;
+  setUserAbility() {
+    this.checkUserAge();
+    this.checkTopicLevel();
+    this.checkUserFeeling();
+    this.calculateLearningAbility();
+  }
+
+  calculateMinutes() {
+    let minPW = this.minutesPerWeek;
+    let maxMinPW = this.answers.maxMinutesPerWeek;
+    let maxUMin = this.maxUserMinutes;
+
+    let minutesMinusLife =
+      minPW - this.answers.lifePerDay * 7 - this.minutesEssential;
+    let minMinusW = minutesMinusLife - this.answers.workPerWeek - 300;
+    if (minMinusW >= maxMinPW) {
+      maxUMin = maxMinPW;
     }
+    if (minMinusW < maxMinPW && minMinusW > 120) {
+      maxUMin = minMinusW;
+    }
+    if (maxUMin <= 120) {
+      maxUMin = 120;
+    }
+    this.maxUserMinutes = maxUMin;
+    console.log('max Minuten die der User insgesamt Zeit hat', maxUMin);
+  }
+
+  calculateMinutesPerDay() {
+    let maxUD = this.maxUserDays;
+    let maxUMinPD = this.maxUserMinutesPerDay;
+    let minPD = this.answers.minutesPerDay;
+
+    maxUD = this.maxDays - this.answers.notPossibleDays.length;
+    console.log('max Tage an denen der User Zeit hat:', maxUD);
+    if (maxUD <= 0) {
+      alert('Hey! There is no day where you have time! Please change that.');
+    }
+    console.log('MAX USER MINUTES:', this.maxUserMinutes);
+    maxUMinPD = this.maxUserMinutes / maxUD;
+    console.log('Max. min pro Tag:', maxUMinPD);
+    if (minPD <= maxUMinPD) {
+      maxUMinPD = minPD;
+      console.log(
+        'User hat max min ausgewählt:',
+        minPD,
+        'Min p. Tag, nach dem Vergleich:',
+        maxUMinPD
+      );
+    }
+  }
+
+  calculateLearningAbility() {
     console.log(
-      'MAX. Stunden die der User insgesamt Zeit hat',
-      this.maxUserHours
+      'USERLEARNINGABILITY VOR BERECHNUNG:',
+      this.userLearningAbility
+    );
+    this.userLearningAbility =
+      this.levelFactor *
+      this.ageFactor *
+      this.feelingSpeed *
+      this.feelingEndurance *
+      this.feelingLove;
+    console.log(
+      'USERLEARNINGABILITY NACH BERECHNUNG:',
+      this.userLearningAbility
     );
   }
 
-  calculateHoursPerDay() {
-    this.maxUserDays = this.maxDays - this.answers.notPossibleDays.length;
-    console.log('MAX. Tage an denen der User Zeit hat:', this.maxUserDays);
-    if (this.maxUserDays <= 0) {
-      alert('Hey! There is no day where you have time! Please change that.');
+  checkUserAge() {
+    let age = this.answers.userAge;
+
+    if (age <= 20) {
+      this.ageFactor = 1.4;
     }
-    this.maxUserHoursPerDay = this.maxUserHours / this.maxUserDays;
-    console.log('Max. Stunden pro Tag:', this.maxUserHoursPerDay);
-    if (this.answers.minutesPerDay <= this.maxUserHoursPerDay) {
-      this.maxUserHoursPerDay = this.answers.minutesPerDay;
-      console.log(
-        'User hat max Minuten ausgewählt:',
-        this.answers.minutesPerDay,
-        'Minuten Pro Tag, nach dem Vergleich:',
-        this.maxUserHoursPerDay
-      );
+    if (age > 20 && age < 30) {
+      this.ageFactor = 1.2;
     }
+    if (age >= 30 && age < 40) {
+      this.ageFactor = 0.9;
+    }
+    if (age >= 40) {
+      this.ageFactor = 0.8;
+    }
+    console.log('FAKTOR ALTER:', this.ageFactor);
+  }
+
+  checkTopicLevel() {
+    let lv = this.answers.level;
+
+    if (lv == 1) {
+      this.levelFactor = 1.5;
+    }
+    if (lv == 2) {
+      this.levelFactor = 1.2;
+    }
+    if (lv == 3) {
+      this.levelFactor = 0.8;
+    }
+    if (lv == 4) {
+      this.levelFactor = 0.6;
+    }
+    console.log('FAKTOR LEVEL', this.levelFactor);
+  }
+
+  checkUserFeeling() {
+    let feelingSpeed = this.answers.learningSlow;
+    let feelingEndurance = this.answers.learningEnduranceGood;
+    let feelingLove = this.answers.learningLove;
+
+    if (feelingSpeed) {
+      this.feelingSpeed = 0.85;
+      console.log('Der Nutzer lernt langsam.');
+    }
+    if (!feelingSpeed) {
+      this.feelingSpeed = 1.1;
+      console.log('Der Nutzer lernt schnell');
+    }
+    if (!feelingEndurance) {
+      this.feelingEndurance = 0.85;
+      console.log('Die Ausdauer des Nutzers ist schlecht.');
+    }
+    if (feelingEndurance) {
+      this.feelingEndurance = 1.1;
+      console.log('Die Ausdauer des Nutzers ist gut.');
+    }
+    if (!feelingLove) {
+      this.feelingLove = 0.85;
+      console.log('Der Nutzer hasst lernen');
+    }
+    if (feelingLove) {
+      this.feelingLove = 1.1;
+      console.log('Der Nutzer liebt lernen');
+    }
+    console.log(
+      'FAKTOR FEELING',
+      this.feelingSpeed,
+      this.feelingEndurance,
+      this.feelingLove
+    );
   }
 
   checkGoodDays() {
@@ -103,25 +218,25 @@ export class PlanComponent implements OnInit {
   checkNotPossibleDays() {
     let nDay = this.answers.notPossibleDays;
     if (nDay.includes('monday')) {
-      this.plan.monday = [];
+      this.plan.monday = ['optional'];
     }
     if (nDay.includes('tuesday')) {
-      this.plan.tuesday = [];
+      this.plan.tuesday = ['optional'];
     }
     if (nDay.includes('wednesday')) {
-      this.plan.wednesday = [];
+      this.plan.wednesday = ['optional'];
     }
     if (nDay.includes('thursday')) {
-      this.plan.thursday = [];
+      this.plan.thursday = ['optional'];
     }
     if (nDay.includes('friday')) {
-      this.plan.friday = [];
+      this.plan.friday = ['optional'];
     }
     if (nDay.includes('saturday')) {
-      this.plan.saturday = [];
+      this.plan.saturday = ['optional'];
     }
     if (nDay.includes('sunday')) {
-      this.plan.sunday = [];
+      this.plan.sunday = ['optional'];
     }
     console.log(this.answers.notPossibleDays);
     console.table(this.plan);
